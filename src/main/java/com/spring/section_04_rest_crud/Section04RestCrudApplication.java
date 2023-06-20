0e7e7a256ac8e0dcaafb1a550608a37da5a24655
comp_theme <iraklitsulukidze14@gmail.com>
2023-06-20 21:52:48 +0400

    dao, service and Controller

---
+++ src/main/java/com/spring/section_04_rest_crud/Section04RestCrudApplication.java
@@ -0,0 +1,13 @@
+package com.spring.section_04_rest_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section04RestCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section04RestCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/section_04_rest_crud/Section04RestCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.section_04_rest_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section04RestCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
